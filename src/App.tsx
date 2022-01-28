5783f3d8dd9438a1d7c86490581fbf9e3edefaa6
Eugeny Shigaev <eugeny85@gmail.com>
2022-01-28 21:43:10 +0400

    Multi TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
 import FirebaseTodoListStore from './stores/FirebaseTodoListStore';
+import TodoListStore from './stores/TodoListStore';
 
 function App() {
     return (
         <div className="App">
-            {/*<TodoList store={new TodoListStore()}/>*/}
+            <TodoList store={new TodoListStore()}/>
             <TodoList store={new FirebaseTodoListStore()}/>
         </div>
     );

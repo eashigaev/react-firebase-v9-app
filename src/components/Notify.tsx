5add9ac78a612387c8e2d9e844702f7a6116b234
Eugeny Shigaev <eugeny85@gmail.com>
2022-01-30 23:13:32 +0400

    Notifications refactoring

--- src/components/Notify.tsx
+++ src/components/Notify.tsx
@@ -1,16 +1,47 @@
-import {onMessage} from "firebase/messaging";
-import React from "react";
+import {getToken, onMessage} from "firebase/messaging";
+import {useEffect, useState} from "react";
 import {messaging} from '../providers/firebase';
+
+// TODO: Fix error
+// FirebaseError: Messaging: A problem occurred while unsubscribing the user from FCM:
+// Requested entity was not found. (messaging/token-unsubscribe-failed). (messaging/token-unsubscribe-failed).
 
 const Notify = () => {
 
-    onMessage(messaging, (payload) => {
-        console.log('Received message', payload);
-    });
+    const [currentToken, setCurrentToken] = useState('');
+
+    const vapidKey = "BCSW6XguEeO0QzGo8myF-B3meQgErWJQFAsUT6HPheMwj1uZ1QAeLEB90g7txUAeTCUEEl7Hmf3YSSd9pxJXXok";
+
+    const onGetTokenSuccess = (currentToken: string) => {
+        if (currentToken) { // Send the token to your server and update the UI if necessary
+            console.log('The registration token is available.', currentToken);
+            setCurrentToken(currentToken);
+        } else { // Show permission request UI
+            console.log('No registration token available. Request permission to generate one.');
+        }
+    };
+    const onGetTokenError = (err: string) => {
+        console.log('An error occurred while retrieving token. ', err);
+    };
+
+    useEffect(() => {
+        getToken(messaging, {vapidKey})
+            .then(onGetTokenSuccess)
+            .catch(onGetTokenError);
+    }, []);
+
+    useEffect(() => {
+        onMessage(messaging, (payload) => {
+            console.log('Received message', payload);
+        });
+    }, []);
 
     return (
         <>
-            <sub>Notification testing...</sub>
+            <sub>
+                {currentToken && 'Notification permission OK'}
+                {!currentToken && 'Need notification permission'}
+            </sub>
         </>
     );
 }
